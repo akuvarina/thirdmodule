111df5aff71ba147ccc4ba4fa3d2016c143159f1
AnnaKuvarina <anya.kuvarinaggmail.com>
2019-04-02 17:04:01 +0300

    new scroll up and navigation

--- js/jquery.js
+++ js/jquery.js
@@ -8,6 +8,17 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 3000
     });
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
 
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
 
 });
