55820b3cb5f43bbb4b54a79a79cb07a8ef55dcdf
Kuvarina, Anna <anna.kuvarina@sigma.software>
2020-03-20 10:27:17 +0200

    update styles

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
 window.onload = function () {
-   let button = document.querySelector('.show-modal');
+    let button = document.querySelector('.show-modal');
     let modal = document.querySelector('#modal-block');
-   button.onclick = function () {
-       modal.classList.add('active');
-       document.body.classList.add('no-scroll');
-   };
-   let close = document.querySelector('#close-modal');
-   close.onclick = function () {
-       modal.classList.remove('active');
-       document.body.classList.remove('no-scroll');
-   };
+    let backup = document.querySelector('.modal-backup');
+
+    button.onclick = function () {
+        modal.classList.add('active');
+        backup.classList.add('modal-backup--active');
+        document.body.classList.add('no-scroll');
+    };
+    let close = document.querySelector('#close-modal');
+    close.onclick = function () {
+        backup.classList.remove('modal-backup--active');
+        modal.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    };
+
+    backup.onclick = function () {
+        backup.classList.remove('modal-backup--active');
+        modal.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    };
 
     let calendar = document.querySelector('input[type="date"]');
-    console.log(calendar);
     let data = new Date();
-   let year = data.getFullYear() - 18;
-   let month = data.getMonth() + 1;
-   let day = data.getDate();
-    if (month < 10){
+    let year = data.getFullYear() - 18;
+    let month = data.getMonth() + 1;
+    let day = data.getDate();
+    if (month < 10) {
         month = '0' + month;
     }
-    if (day < 10){
+    if (day < 10) {
         day = '0' + day;
     }
     let maxDay = `${year}-${month}-${day}`;
-   calendar.setAttribute('max', maxDay);
+    calendar.setAttribute('max', maxDay);
 };
